68af88bd681c222d1814dcd92e81cf16e0bd7771
Hassan <123646068+Hassanjabbar1212@users.noreply.github.com>
2024-02-01 12:18:37 +0500

    Add files via upload

---
+++ Controller/HomeController.cs
@@ -0,0 +1,171 @@
+﻿using Medical.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+using Medical.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Mail;
+using System.Net;
+
+namespace Medical.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Context _context;
+        public HomeController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(User loginPage)
+        {
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.Email == loginPage.Email && u.Password == loginPage.Password);
+
+            if (user != null)
+            { 
+                var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Email),
+                new Claim(ClaimTypes.Sid, Convert.ToString(user?.Id)),
+                // Add other claims as needed
+            };
+
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var authProperties = new AuthenticationProperties
+                {
+                    AllowRefresh = true,
+                    IsPersistent = true,
+                };
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+                return RedirectToAction("Profile", "Home");
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Register(User user)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+        [HttpGet]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ChangePassword(User password1)
+        {
+            var logedInId = User.FindFirstValue(ClaimTypes.Sid);
+
+            if (int.TryParse(logedInId, out int userId))
+            {
+                var user = _context.Users.FirstOrDefault(a => a.Id == userId);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    // Update the existing user's password
+                    user.Password = password1.Password;
+
+                    // Save the changes to the database
+                    _context.SaveChanges();
+                }
+            }
+
+            return View();
+
+        }
+        public IActionResult Profile()
+        {
+
+            return View();
+        }
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ForgotPassword(string Email)
+        {
+            if (string.IsNullOrWhiteSpace(Email))
+            {
+                ModelState.AddModelError("Email", "Email is required.");
+                return RedirectToAction("Index", "Home"); // Display the form again with an error message.
+            }
+
+            // Check if the email exists in your user database.
+            // If it exists, generate a random password and send it via email.
+
+            var user = _context.Users.FirstOrDefault(u => u.Email == Email);
+
+            if (user != null)
+            {
+                // Generate a random password
+                //const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                //var random = new Random();
+                //var randomPassword = new string(Enumerable.Repeat(chars, 12)
+                //    .Select(s => s[random.Next(s.Length)]).ToArray());
+
+                //// Update the user's password in the database with the random password.
+                //user.Password = randomPassword;
+
+
+                //_context.SaveChanges();
+                var password = user.Password;
+                var smtpClient = new SmtpClient("smtp.gmail.com")
+                {
+                    Port = 587,
+                    Credentials = new NetworkCredential("hassanjabbar2017@gmail.com", "poljeaszpdjiywtk"),
+                    EnableSsl = true,
+                };
+
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress("hassanjabbar2017@gmail.com"), // Replace with your Gmail email address.
+                    Subject = "Your Password",
+                    Body = $"Your password is: {password}",
+                    IsBodyHtml = false,
+                };
+
+                mailMessage.To.Add(Email); // Use the provided email as the recipient.
+
+                smtpClient.Send(mailMessage);
+
+                return RedirectToAction("Login", "Home");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Controller/Item.cs
@@ -0,0 +1,250 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Medical.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Medical.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Medical.Controllers
+{
+    public class Item : Controller
+    {
+        private readonly Context _context;
+        public Item(Context context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult List()
+        {
+         var items = _context.Items
+      
+        .Select(i => new ItemViewModel
+     {
+         Id = i.Id,
+         MgId = i.Mgs != null ? i.Mgs.Id : (int?)null,
+         MgName = i.Mgs != null ? i.Mgs.MgName : "",
+         Name = i.Name,
+         IsBox = true,
+         Quantity = i.Quantity,
+         BoxPrice = i.BoxPrice,
+         SinglePrice = i.SinglePrice,
+     })
+     .ToList();
+            return View(items);
+        }
+        
+        public IActionResult Index()
+        {
+            var items = _context.Items
+     .Where(i => i.Isbox == true)
+     .Select(i => new ItemViewModel
+     {
+         Id = i.Id,
+         MgId = i.Mgs != null ? i.Mgs.Id : (int?)null,
+         MgName = i.Mgs != null ? i.Mgs.MgName : "",
+         Name = i.Name,
+         IsBox = true,
+         Quantity = i.Quantity,
+         BoxPrice = i.BoxPrice,
+         SinglePrice = i.SinglePrice,
+     })
+     .ToList();
+            return View(items);
+        }
+        [HttpGet]
+        public IActionResult Single()
+        {
+            var items = _context.Items
+    .Where(i => i.Isbox==false) 
+    .Select(i => new ItemViewModel
+    {
+        Id = i.Id,
+        MgId = i.Mgs != null ? i.Mgs.Id : (int?)null,
+        MgName = i.Mgs != null ? i.Mgs.MgName : "",
+        Name = i.Name,
+        IsBox = false,
+        Quantity = i.Quantity,
+        BoxPrice = i.BoxPrice,
+        SinglePrice = i.SinglePrice,
+    })
+    .ToList();
+            return View(items);
+        }
+        [HttpGet]
+        public IActionResult AddItem()
+        {
+            ViewBag.MgList = new SelectList(_context.mgs.ToList(), "Id", "MgName");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddItem(Items item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+
+        public IActionResult EditItem(int? id)
+        {
+            var content = _context.Items
+    .Include(i => i.Mgs)
+    .FirstOrDefault(a => a.Id == id);
+
+            if (content == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                // Assuming Mg has a property named 'Name'
+                var mgName = content.Mgs.MgName;
+
+                // You can create a new model or use ViewBag to pass additional data to the view
+                // For simplicity, let's assume you're using ViewBag
+                ViewBag.MgName = mgName;
+
+                return View(content);
+            }
+        }
+        public IActionResult EditItems(Items updatedItems)
+        {
+            var existingItem = _context.Items.FirstOrDefault(i => i.Id == updatedItems.Id);
+
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+
+            // Update properties of the existing item
+            existingItem.Name = updatedItems.Name;
+            existingItem.Mgs = updatedItems.Mgs;
+            existingItem.SinglePrice = updatedItems.SinglePrice;
+            existingItem.BoxPrice = updatedItems.BoxPrice;
+            existingItem.Quantity = updatedItems.Quantity;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            var content = _context.Items
+    .Include(i => i.Mgs)
+    .FirstOrDefault(a => a.Id == id);
+
+            if (content == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                // Assuming Mg has a property named 'Name'
+                //var mgName = content.Mgs.MgName;
+
+                //// You can create a new model or use ViewBag to pass additional data to the view
+                //// For simplicity, let's assume you're using ViewBag
+                //ViewBag.MgName = mgName;
+
+                return View(content);
+            }
+        }
+        public IActionResult DeleteItem(int id)
+        {
+            var itemToDelete = _context.Items.FirstOrDefault(i => i.Id == id);
+
+            if (itemToDelete == null)
+            {
+                return NotFound();
+            }
+
+            _context.Items.Remove(itemToDelete);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+
+        public IActionResult SalesLog()
+        {
+
+            var items = _context.Items.ToList();
+            ViewBag.Items = new SelectList(items, "Id", "Name");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SalesLog(SalesLog salesLog)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save the sales log entry to the database or any other storage mechanism
+                var salesLogEntry = new SalesLog
+                {
+                    SaleDate = DateTime.Now,
+                    ItemId = salesLog.ItemId,
+                    QuantitySold = salesLog.QuantitySold,
+                    IsFullBox = salesLog.IsFullBox,
+                };
+
+                _context.SalesLog.Add(salesLogEntry);
+                _context.SaveChanges();
+                // Update the item quantity in the Items table
+                var item = _context.Items.FirstOrDefault(i => i.Id == salesLog.ItemId);
+                if (item != null)
+                {
+                    // Optionally, you can also update the box quantity based on IsFullBox
+                    if (salesLog.IsFullBox==true)
+                    {
+                        if(item.Isbox==true)
+                        {
+                            item.Quantity -= salesLog.QuantitySold;
+                        } 
+                    }
+                    else
+                    {
+                        item.Quantity -= salesLog.QuantitySold;
+                    }
+
+                    _context.SaveChanges();
+                }
+
+                // Redirect to the sales log page or any other page
+                return RedirectToAction("SalesLog");
+            }
+
+            // If the model state is not valid, return to the same view with validation errors
+            return View();
+        }
+        [HttpGet]
+        public IActionResult SoldItems()
+        {
+            var soldItems = _context.SalesLog
+                .Select(i => new SoldViewModel
+                {
+                   ItemName = i.Items.Name,
+                   QuantitySold = i.QuantitySold,
+                   IsFullBox = i.IsFullBox,
+                   SaleDate = i.SaleDate,
+                })
+     .ToList();
+
+                
+            return View(soldItems);
+        }
+        [HttpGet]
+
+        public IActionResult AddMG()
+        {
+            return View();
+        }
+        public IActionResult AddMG(Mg mg)
+        {
+            _context.mgs.Add(mg);
+            _context.SaveChanges();
+            return View();
+        }
+    }
+}
